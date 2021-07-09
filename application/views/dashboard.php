98a3d8e28fa41f1621ac3baf64d9d584f2f83142
Ricky Leonardo Wijaya <ricky250100@gmail.com>
2021-07-09 13:56:22 +0700

    commit

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -114,16 +114,16 @@
                     <div class="row">
                         <?php foreach($ruangan as $ruang): ?>
                             <div class="col-xl-3 col-md-6 mb-4">
-                                <div class="card border-left-<?php echo ($ruang["waktu_keluar"] == null) ? 'success' : 'danger' ?> shadow h-100 py-2">
+                                <div class="card border-left-<?php echo ($ruang["waktu_keluar"] != null) ? 'success' : 'danger' ?> shadow h-100 py-2">
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
                                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                                     <?php echo $ruang["ruangan"] ?></div>
-                                                <div class="h6 mb-0 text-gray-800"><?php echo ($ruang["waktu_keluar"] == null) ? 'Ruangan Bisa Dipakai' : 'Ruangan Sedang Dipakai' ?></div>
+                                                <div class="h6 mb-0 text-gray-800"><?php echo ($ruang["waktu_keluar"] != null) ? 'Ruangan Bisa Dipakai' : 'Ruangan Sedang Dipakai' ?></div>
                                             </div>
                                             <div class="col-auto">
-                                                <i class="fas fa-door-<?php echo ($ruang["waktu_keluar"] == null) ? 'open' : 'closed' ?> fa-2x text-gray-300"></i>
+                                                <i class="fas fa-door-<?php echo ($ruang["waktu_keluar"] != null) ? 'open' : 'closed' ?> fa-2x text-gray-300"></i>
                                             </div>
                                         </div>
                                     </div>
